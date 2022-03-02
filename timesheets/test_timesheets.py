d8de755200306c010e177f693cef949381cf6c69
Dan Smestad <hockeydan25@gmail.com>
2022-03-02 17:47:31 -0600

    unitest og = origional unittest program. I added another testing to with 11 passing test with instruction w video examples.

--- timesheets/test_timesheets.py
+++ timesheets/test_timesheets.py
@@ -1,102 +1,91 @@
-import unittest 
+import unittest
 from unittest import TestCase
 from unittest.mock import patch, call
 
 import timesheets
 
-class TestTimeSheet(TestCase):
+class TestTimesheet(TestCase):
+    """ mock the user input and for it to return a value to use for testing 
+        test logic funtions, finish total hours. mock minimum.
+    """
 
-    # Mock the print function, check it was called with expected text"""
+    @patch('builtins.input', side_effect=['2'])  # list a mock input here though side_effct, could be a list of inputs tried.
+    def test_get_hours_for_day(self, mock_input):
+        hours = timesheets.get_hours_for_day('Monday')  # testing the input is checking correctly housrs entered.
+        self.assertEqual(2, hours) 
+
+
+    @patch('builtins.input', side_effect=['cat', 'fish', '', '123birdy', 'pizza1234',  '2'])  # list of mock inputs here
+    def test_get_hours_for_day_non_numeric(self, mock_input):
+        hours = timesheets.get_hours_for_day('Monday')  # testing the input is checking correctly housrs entered.
+        self.assertEqual(2, hours)     
+
+    @patch('builtins.input', side_effect=[ '-12', '-1234',  '8'])  # list of mock neagative inputs here
+    def test_get_hours_for_day_number_range_negative(self, mock_input):
+        hours = timesheets.get_hours_for_day('Monday')  # testing the input is checking correctly housrs entered.
+        self.assertEqual(8, hours)  
+   
+
+    @patch('builtins.input', side_effect=[ '120', '1234',  '8'])  # list of mock inputs here
+    def test_get_hours_for_day_range_greater_than_24(self, mock_input):
+        hours = timesheets.get_hours_for_day('Monday')  # testing the input is checking correctly housrs entered.
+        self.assertEqual(8, hours)  
+
+
+    @patch('builtins.input', side_effect=[ '24.000001', '1234',  '8'])  # list of mock inputs here
+    def test_get_hours_for_day_range_decimal_greater_than_24(self, mock_input):
+        hours = timesheets.get_hours_for_day('Monday')  # testing the input is checking correctly housrs entered.
+        self.assertEqual(8, hours)  
     
-    @patch('builtins.print')
+
+    @patch('builtins.print')  # builtins ? 
     def test_display_total(self, mock_print):
         timesheets.display_total(123)
-        mock_print.assert_called_once_with('Total hours worked: 123')
+        mock_print.assert_called_once_with('Total hours worked: 123')  # testing the print function display
 
 
-    # Mock input() to force it to return '2' 
-
-    @patch('builtins.input', side_effect=['2'])
-    def test_get_hours_for_day(self, mock_input):
-        hours = timesheets.get_hours_for_day('Monday')
-        self.assertEqual(2, hours)
+    @patch('timesheets.alert') 
+    def test_alert_meet_min_hours_doesnt_meet(self, mock_alert):
+        timesheets.alert_not_meet_min_hours(12, 30)  
+        mock_alert.assert_called_once()  # testing now that the aleart is called once. is alerting
 
 
-    # Verify the get_hours_for_day function ignores non-numerical input 
+    @patch('timesheets.alert') 
+    def test_alert_meet_min_hours_does_meet(self, mock_alert):
+        timesheets.alert_not_meet_min_hours(40, 30)  
+        mock_alert.assert_not_called()  # testing now that the aleart is not called, not alerting.   
 
-    @patch('builtins.input', side_effect=['cat', '123pizza', '2.4'])
-    def test_get_hours_for_day_validation(self, mock_input):
-        hours = timesheets.get_hours_for_day('whatever')
-        self.assertEqual(2.4, hours)
+    @patch('timesheets.get_hours_for_day')
+    def test_get_hours(self, mock_get_hours):       
+        mock_hours = [5, 7, 9]  # list of mock hours, example 
+        mock_get_hours.side_effect = mock_hours  # will help make assertions with example data
+        days = ['g', 'x', 'k']  # variable days data is fake data like a placeholder
+        expected_hours = dict(zip(days, mock_hours))  # expected output: dict funct and zip funct creates a dictionary of {'m': 5, 't': 7, 'w': 9}
+        hours = timesheets.get_hours(days)  # days arguments is use to give data to hours  
+        self.assertEqual(expected_hours, hours)  # test checking arguments 
 
-
-    # Verify the get_hours_for_day function rejects numbers less than 0
-
-    @patch('builtins.input', side_effect=['-1', '-1000', '6'])
-    def test_get_hours_for_day_hours_greater_than_zero(self, mock_input):
-        hours = timesheets.get_hours_for_day('Monday')
-        self.assertEqual(6, hours)
-
-
-    # Verify the get_hours_for_day function rejects numbers greater than 24
-
-    @patch('builtins.input', side_effect=['24.00000000001', '1000', '25', '9'])
-    def test_get_hours_for_day_hours_less_than_24(self, mock_input):
-        hours = timesheets.get_hours_for_day('Monday')
-        self.assertEqual(9, hours)
-
-
-    # Mock the get_hours_for_day function and return predetermined numbers
-
-    @patch('timesheets.get_hours_for_day', side_effect=[5, 7, 9])
-    def test_get_hours(self, mock_get_hours):
-        days = ['m', 't', 'w']
-        expected_hours = dict(zip(days,[5, 7,9]))        
-        hours = timesheets.get_hours(days)
-
-        self.assertDictEqual(expected_hours, hours)
-
-
-    #Mock the print function, check the correct calls are made in the expected order
 
     @patch('builtins.print')
     def test_display_hours(self, mock_print):
-        example = {'M': 3, 'T': 12, 'W': 8.5}
-
+        #arrange
+        example = {'M': 3, 'T': 12, 'W': 8.5}  
         expected_table_calls = [
             call('Day            Hours Worked   '),
-            call('M              3              '),
+            call('M              3              '),  # 15 spaces like the function in timesheets.
             call('T              12             '),
-            call('W              8.5            ')
-        ]
-        
-        timesheets.display_hours(example)
-        mock_print.assert_has_calls(expected_table_calls)
+            call('W              8.5            '),
+        ]    # tuple of data makes up a mock chart
+        # action
+        timesheets.display_hours(example)  #call to example for data
+        mock_print.assert_has_calls(expected_table_calls)  # function call to ptint table or chart.
 
-
-    # This is an easy function to test - no mocking needed! 
-    
+       
     def test_total_hours(self):
-        example = {'M': 3, 'T': 12, 'W': 8.5}
+        example = {'M': 3, 'T': 12, 'W': 8.5} 
         total = timesheets.total_hours(example)
-        expected_total = 3 + 12 + 8.5 
+        expected_total = 3 + 12 +8.5  # hours in example should be expeceted.  
         self.assertEqual(total, expected_total)
 
-
-    # Mock the alert function so no actual beeping needed 
-    
-    @patch('timesheets.alert')
-    def test_alert_meet_min_hours_doesnt_meet(self, mock_alert):
-        timesheets.alert_not_meet_min_hours(12, 30)
-        mock_alert.assert_called_once()   # beep expected
-
-
-    @patch('timesheets.alert')
-    def test_alert_meet_min_hours_exceed(self, mock_alert):
-        timesheets.alert_not_meet_min_hours(45, 30)
-        mock_alert.assert_not_called()   # no beep expected 
-
-
-
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()        
+
